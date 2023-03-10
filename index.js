228b7882bb5a79a9fbf8f71652c504fccfb74010
2bit2bit <emharuna2020@gmail.com>
2023-03-10 11:32:15 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+let items = JSON.parse(
+  fs.readFileSync(
+    path.join(
+      __dirname,
+      "List of States and Local Government Areas (LGAs) in Nigeria.json"
+    ),
+    "utf8"
+  )
+);
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(JSON.stringify(items));
+    res.end();
+  })
+  .listen(3000);
+
+  
